5e249a7eb9441a2cb1fcce95fa5a2b3527cdc8c3
Krista DeStasio <kdestasio@gmail.com>
2018-10-10 09:44:48 -0700

    configure to CAPS specifications

--- org/bidsQC/conversion/config_dcm2bids_helper.py
+++ org/bidsQC/conversion/config_dcm2bids_helper.py
@@ -9,31 +9,33 @@
 # https://github.com/kdestasio/dcm2bids
 
 import os
+from datetime import datetime
+
 # Set study info (may need to change for your study)
 # These variables are used only in this file for paths. Can omit if wanted.
-group = "sanlab"
-study = "REV"
+group = 'sanlab'
+study = 'CAPS'
 
 
 # The following variables are used in the main script and need to be defined here. 
 # They need to exist prior to running the script
 
 # Directories
-parentdir = os.path.join(os.sep, "projects", group, "shared", study) # folder that contains bidsdir and codedir
-dicomdir = os.path.join(os.sep, "projects", "shared", "DICOMS")
-bidsdir = os.path.join(parentdir, "bids_data") # where the niftis will be put
-codedir = os.path.join(parentdir, "REV_study", "org", "dcm2bids") # Contains subject_list.txt, config file, and dcm2bids_batch.py
-logdir = os.path.join(codedir, "logs_helper")
+parentdir = os.path.join(os.sep, 'projects', group, 'shared', study) # folder that contains bidsdir and codedir
+dicomdir = os.path.join(os.sep, 'projects', 'lcni', 'dcm', 'sanlab', 'Berkman', study)
+bidsdir = os.path.join(parentdir, 'bids_data') # where the niftis will be put
+codedir = os.path.join(parentdir, 'CAPS_scripts', 'org', 'bidsQC', 'conversion') # Contains subject_list.txt, config file, and dcm2bids_batch.py
+logdir = os.path.join(codedir, 'logs_helper')
 
 # Log files
-outputlog = os.path.join(logdir, "outputlog_helper.txt")
-errorlog = os.path.join(logdir, "errorlog_helper.txt")
+outputlog = os.path.join(logdir, 'outputlog_helper' + datetime.now().strftime('%Y%m%d-%H%M') + '.txt')
+errorlog = os.path.join(logdir, 'errorlog_helper' + datetime.now().strftime('%Y%m%d-%H%M') + '.txt')
 
 # Test subject
-test_subject = "S001" # Name of a directory that contains DICOMS for one participant
+test_subject = 'CAPS108_20180927_151529' # Name of a directory that contains DICOMS for one participant
 
 # Run on local machine (run_local = True) or high performance cluster with slurm (run_local = False)
 run_local = True
 
-# If false, set the singularity image. Else, set to "NA"
-singularity_image =  os.path.join(os.sep, "projects", group, "shared", "containers", "Dcm2Bids-master.simg")
+# If false, set the singularity image. Else, set to 'NA'
+singularity_image =  os.path.join(os.sep, 'projects', group, 'shared', 'containers', 'Dcm2Bids-master.simg')
