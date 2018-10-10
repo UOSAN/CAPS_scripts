7b9fb9327b4d318555d80aaa5ad5ef9cc1c65c07
Krista DeStasio <kdestasio@gmail.com>
2018-10-10 10:11:10 -0700

    specify for CAPS

--- org/bidsQC/conversion/config_dcm2bids_batch.py
+++ org/bidsQC/conversion/config_dcm2bids_batch.py
@@ -22,31 +22,31 @@ from datetime import datetime
 # Set study info 
 # These variables are used only in the config file path names 
 # and can be commented out if not needed.
-group = "sanlab"
-study = "REV"
-parent_dir = "dcm2bids"
+group = 'sanlab'
+study = 'CAPS'
+parent_dir = 'bidsQC'
 
 
 # Set directories
 # These variables are used in the main script and need to be defined here.
 # They need to exist prior to running the script (with the exception of `image` 
-# which can be set equal to "NA" if you are running the script locally)
-dicomdir = os.path.join(os.sep, "projects", "lcni", "dcm", group, "Archive", study)
-codedir = os.path.join(os.sep, "projects", group, "shared", study, "REV_scripts", "org", parent_dir)  # Contains subject_list.txt, config file, and dcm2bids_batch.py
-configfile = os.path.join(codedir, "study_config.json")  # path to and name of config file
-image = os.path.join(os.sep, "projects", group, "shared", "containers", "Dcm2Bids-master.simg")
+# which can be set equal to 'NA' if you are running the script locally)
+dicomdir = os.path.join(os.sep, 'projects', 'lcni', 'dcm', group, 'Berkman', study)
+codedir = os.path.join(os.sep, 'projects', group, 'shared', study, 'CAPS_scripts', 'org', parent_dir, 'conversion')  # Contains subject_list.txt, config file, and dcm2bids_batch.py
+configfile = os.path.join(codedir, 'study_config.json')  # path to and name of config file
+image = os.path.join(os.sep, 'projects', group, 'shared', 'containers', 'Dcm2Bids-master.simg')
 
 # These variables are also used in the main script and need to be defined here.
 # If they don't exist, they will be created by the script
-niidir = os.path.join(os.sep, "projects", group, "shared", study, "bids_data") # Where the niftis will be put
-logdir = os.path.join(codedir, "logs_dcm2bids")
-outputlog = os.path.join(logdir, "outputlog_dcmn2bids" + datetime.now().strftime("%Y%m%d-%H%M") + ".txt")
-errorlog = os.path.join(logdir, "errorlog_dcm2bids" + datetime.now().strftime("%Y%m%d-%H%M") + ".txt")
+niidir = os.path.join(os.sep, 'projects', group, 'shared', study, 'bids_data') # Where the niftis will be put
+logdir = os.path.join(codedir, 'logs_dcm2bids')
+outputlog = os.path.join(logdir, 'outputlog_dcmn2bids' + datetime.now().strftime('%Y%m%d-%H%M') + '.txt')
+errorlog = os.path.join(logdir, 'errorlog_dcm2bids' + datetime.now().strftime('%Y%m%d-%H%M') + '.txt')
 
 
 # Source the subject list (needs to be in your current working directory)
-subjectlist = "subject_list.txt"
+subjectlist = 'subject_list.txt'
 
 
 # Run on local machine (run_local = True) or high performance cluster with slurm (run_local = False)
-run_local = True
+run_local = False
